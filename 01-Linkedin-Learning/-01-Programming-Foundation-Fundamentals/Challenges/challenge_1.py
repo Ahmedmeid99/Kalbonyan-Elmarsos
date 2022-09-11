c616b1aae83ddf145964c2cfe042635df708337f
ahmed mo-Eid <89750867+Ahmedmeid99@users.noreply.github.com>
2022-09-11 18:59:01 +0200

    Add files via upload

---
+++ 01-Linkedin-Learning/-01-Programming-Foundation-Fundamentals/Challenges/challenge_1.py
@@ -0,0 +1,13 @@
+# 
+# >>>>>>>> Guessing game
+# 
+myFavorite_Food = "meat"
+answer = input("what is my favorite food ?")
+
+if answer == myFavorite_Food:
+    print("Yep! So amazing!")
+else: 
+    print("Yuck! That`s not it!")
+
+
+print("Thanks for playing")
---
+++ 01-Linkedin-Learning/-01-Programming-Foundation-Fundamentals/Challenges/challenge_2.py
@@ -0,0 +1,29 @@
+#
+#  if  condition :
+#    block code
+# else :
+#     block code2
+
+print("Hi !")
+
+name = input(" whit is your name ? ")
+print("it`s nice to meet you,",name)
+
+answer= input("Do you like python ? ")
+if answer == "yes":
+    print("me too")
+else:
+    print("so what is your favorite programming language ? ")
+#-------------------------------------------------#
+if 4 > 5 :
+    print("true 4 > 5")
+else:
+    print("false 4 < 5")
+#-------------------------------------------------#
+my_string ="Ahmed Eid"
+# my_string ="Ahmed Ali"
+
+if my_string == "Ahmed Eid":
+    print("Hi Ahmed Eid")
+else:print("Hi! ...")
+#-------------------------------------------------#
