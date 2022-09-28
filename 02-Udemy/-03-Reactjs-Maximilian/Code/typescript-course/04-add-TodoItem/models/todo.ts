aed84b48ac879280eb3ccfa79b0e0c8e314e8861
ahmed mo-Eid <89750867+Ahmedmeid99@users.noreply.github.com>
2022-09-28 18:59:21 +0200

    Delete 02-Udemy/-03-Reactjs-Maximilian/Code/typescript-course/04-add-TodoItem directory

--- 02-Udemy/-03-Reactjs-Maximilian/Code/typescript-course/04-add-TodoItem/models/todo.ts
+++
@@ -1,9 +0,0 @@
-class Todo {
-  text: string;
-  id: string;
-  constructor(todoText: string) {
-    this.text = todoText;
-    this.id = `${todoText}${new Date().toISOString()}`;
-  }
-}
-export default Todo;
